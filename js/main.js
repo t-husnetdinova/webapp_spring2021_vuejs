bfb56c165b0bb21148cd395d89771e45b98ed8af
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-03-05 09:01:51 -0700

    created and mounted vue app

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// create vue app
+const app = Vue.createApp({
+    data() {
+        return {
+            keyword: '',
+            result: null
+        }
+    },
+    methods: {
+        searchGoogleBooks() {
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + txtSearch.value + "&startIndex=0&maxResults=20")
+            .then(response => response.json())
+            .then(json => { this.result = json})       
+        }
+    }
+})
