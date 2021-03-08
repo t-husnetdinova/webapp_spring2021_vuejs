49c2f2417bc9412126a9b300366f6e09ef87cba0
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-03-07 21:19:21 -0700

    added bookDisplay component

--- js/main.js
+++ js/main.js
@@ -8,25 +8,9 @@ const app = Vue.createApp({
     },
     methods: {
         searchGoogleBooks() {
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + txtSearch.value + "&startIndex=0&maxResults=20")
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=0&maxResults=20")
             .then(response => response.json())
-            .then(json => { 
-                this.result = json;
-                var list = `<div class="row col-sm-5">found ${json.totalItems} books</div> 
-                <div class="row">`
-                for(let i = 0; i < json.items.length; i++) {
-                    console.log(json.items[i].volumeInfo.title)
-                    var bookInfo = json.items[i].volumeInfo;
-                    var template = `<ul class="col-sm-12 col-md-6 col-xl-4:>`;
-                    template += `<a class="list-group-item list-group-item-action active" href=${json.items[i].selfLink}">${bookInfo.title}</a>`
-                    template += `<li class="list-group-item">${bookInfo.authors}</li>`
-                    template += `<li class="list-group-item">${bookInfo.publisher}, ${bookInfo.publishedDate}</li>`
-                    template += `</ul>`
-                    list += template;
-                }
-                list += `</div>`
-                this.result = list
-            })       
+            .then(json => this.result = json)       
         }
     }
 })
